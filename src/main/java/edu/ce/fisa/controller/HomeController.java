0f197ccb97255238ea6adb7f3e5c27eadfedb1a8
ddddabi <ii887742@gmail.com>
2025-09-16 10:29:40 +0900

    feat: step04 gradle build project with GET/POST

---
+++ src/main/java/edu/ce/fisa/controller/HomeController.java
@@ -0,0 +1,20 @@
+package edu.ce.fisa.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/hello")
+    public String getHello() {
+        return "Hello GET!";
+    }
+
+    @PostMapping("/hello")
+    public String postHello(@RequestBody String name) {
+        return "Hello POST! " + name;
+    }
+}
